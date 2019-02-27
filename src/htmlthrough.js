5349cab1d6695ca1670a1d26a6a8a141a4e38ba0
unknown <mregydev@gmail.com>
2019-02-27 23:23:22 +0300

    added stuff

--- src/htmlthrough.js
+++ src/htmlthrough.js
@@ -1,22 +1,21 @@
-import { stopWords } from './stopwords'; 
-import {Transform} from 'stream';
+import { stopWords } from './stopwords'
+import { Transform } from 'stream'
 export class HtmlThrough extends Transform {
-    constructor() {
-        super({ writableObjectMode: true });
-        this.tokens = new Map();
-    }
+  constructor () {
+    super({ writableObjectMode: true })
+    this.tokens = new Map()
+  }
 
-    _transform(chunk, encoding, next) {
-        if (!chunk || !chunk.length) {
-            next(new Error("chunk is in valid"));
-        }
-        else if (chunk.length == 2 && chunk[0] == 'text') {
-            //apply stemmer
-            let tokens = chunk[1].toString().split(/\s+/);
+  _transform (chunk, encoding, next) {
+    if (!chunk || !chunk.length) {
+      next(new Error('chunk is in valid'))
+    } else if (chunk.length === 2 && chunk[0] === 'text') {
+      // apply stemmer
+      let tokens = chunk[1].toString().split(/\s+/)
 
-            //Increment frequency
-            tokens.forEach((token) => token && token.trim() && stopWords.indexOf(token) == -1 ? this.tokens.set(token, this.tokens.get(token) + 1 || 1) : false);
-        }
-        next();
+      // Increment frequency
+      tokens.forEach((token) => token && token.trim() && stopWords.indexOf(token) === -1 ? this.tokens.set(token, this.tokens.get(token) + 1 || 1) : false)
     }
+    next()
+  }
 }
--- src/indexerstream.js
+++ src/indexerstream.js
@@ -1,36 +1,35 @@
 
-import {Readable} from 'stream';
+import { Readable } from 'stream'
 
 export class IndexerStream extends Readable {
-    constructor(tokens, chunkSize) {
-        super({ objectMode: true });
-        this.chunkSize = chunkSize;
-        this.tokens = tokens;
-        this.counter = 0;
-        this.size = this.tokens.size;
+  constructor (tokens, chunkSize) {
+    super({ objectMode: true })
+    this.chunkSize = chunkSize
+    this.tokens = tokens
+    this.counter = 0
+    this.size = this.tokens.size
+  }
+
+  _read () {
+    if (this.counter > this.size) {
+      this.push(null)
+      return
     }
 
-    _read() {
+    let endIndex = this.counter + this.chunkSize
 
-        if (this.counter > this.size) {
-            this.push(null);
-            return;
-        }
+    let termsToPush = []
+    while (this.counter < endIndex) {
+      let term = this.tokens.keys().next().value
+      let freq = this.tokens.get(term)
 
-        let endIndex = this.counter + this.chunkSize;
-
-        let termsToPush = [];
-        while (this.counter < endIndex) {
-            let term = this.tokens.keys().next().value;
-            let freq = this.tokens.get(term);
-
-            if (term && freq) {
-                termsToPush.push({ term, freq, isFirstChunk: this.counter == 0, isLastChunk: this.counter + 1 >= this.size });
-                this.tokens.delete(term);
-            }
-            this.counter++;
-        }
-
-        this.push(termsToPush);
+      if (term && freq) {
+        termsToPush.push({ term, freq, isFirstChunk: this.counter === 0, isLastChunk: this.counter + 1 >= this.size })
+        this.tokens.delete(term)
+      }
+      this.counter++
     }
+
+    this.push(termsToPush)
+  }
 }
